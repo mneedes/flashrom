5069deb0f6faa39107504925653f22a4c886e8ac
Matt Needes <mneedes@gmail.com>
2021-01-12 13:04:08 -0800

    Add support for Macronix MX25R1635F Usability improvements for run_flashrom.sh
    Change-Id: Ia4a3f708f26733dd078059a02306a81ee98bf500
    signed-off-by: Matthew Needes <mneedes@roku.com>

--- run_flashrom.sh
+++ run_flashrom.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-# Uses Flashrom utility from github, chip definitions at bottom of file
+# NOTE: Uses Flashrom utility from github
 
-#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=8 --layout rom.layout --image sector1 --read funXV.bin
-#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=8 --layout rom.layout --image sector1 --write funX.bin
-#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=16 -c XT25W32B --write READ1.bin
+#
+# Choose (uncomment) chip below
+#
+
+# 2MB Macronix (MXIC)
+#CHIP="MX25R1635F"
+
+# 4MB Macronix (MXIC)
+#CHIP="MX25R3235F"
+
+# 4MB XTX
+#CHIP="XT25W32B"
 
 FTDI_ARGS="-p ft2232_spi:type=4232H,port=B,divisor=8"
-CHIP="-c MX25R3235F"
-#CHIP="-c XT25W32B"
-ARGS="$FTDI_ARGS $CHIP"
 
 # Identify
 if [ "$1" == "identify" ]; then
     sudo ./flashrom -VV $FTDI_ARGS
+    exit 0
 fi
+
+if [ "$CHIP" == "" ]; then
+    echo "Must specify CHIP before running script in erase/write mode"
+    exit -1
+fi
+
+ARGS="$FTDI_ARGS -c $CHIP"
 
 # Erase
 if [ "$1" == "erase" ]; then
@@ -30,3 +44,7 @@ if [ "$1" == "write" ]; then
     sudo ./flashrom $ARGS --read READ1.bin
     diff READ1.bin $2
 fi
+
+#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=8 --layout rom.layout --image sector1 --read funXV.bin
+#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=8 --layout rom.layout --image sector1 --write funX.bin
+#sudo ./flashrom -p ft2232_spi:type=4232H,port=B,divisor=16 -c XT25W32B --write READ1.bin
